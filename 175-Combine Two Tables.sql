4cbabc87461aa8559582c7d1c1f002ad3ce5094e
Garima-Anand1003 <garima.anand2003@gmail.com>
2025-07-04 02:43:17 +0530

    Create 175-Combine Two Tables.sql
    Added solution to a leetcode problem.

---
+++ 175-Combine Two Tables.sql
@@ -0,0 +1,9 @@
+#Leetcode Problem link->https://leetcode.com/problems/combine-two-tables/description/
+
+select  p.firstName,
+p.lastName,
+a.city,
+a.state
+from person p
+left join address a
+on  p.personId = a.personId;
